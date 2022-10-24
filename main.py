c9180527c0882e227574690a2a3163be887f9262
Víctor <113469901+uShawnTS@users.noreply.github.com>
2022-10-23 21:55:42 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+print('''
+YouTube Channel ID Scraper
+Example: https://www.youtube.com/c/SomeChannel
+''')
+
+inputUser = str(input('Type the URL of some YouTube channel to know its channel ID: '))
+try:
+    result = requests.get(inputUser)
+    soup = BeautifulSoup(result.text, 'lxml')
+
+    # Channel information
+    title = soup.findAll(property='og:title')
+    channelId = soup.findAll(property='og:url')
+
+    print(f'''
+    Name: {' '.join(str(e) for e in title).split('content=')[1].replace(' property="og:title"/>', '')}
+    Channel ID: {' '.join(str(e) for e in channelId).split('content=')[1].replace(' property="og:url"/>', '')}
+    ''')
+except ValueError:
+    print('Error: Must provide an URL from youtube.com to get YouTube Channel ID.\nExample: https://www.youtube.com/c/SomeChannel');
+except IndexError:
+    print('Error: YouTube Channel ID not found. Make sure that URL is from youtube.com\nExample: https://www.youtube.com/c/SomeChannel')
