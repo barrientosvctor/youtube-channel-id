c11ef9cb3d470d5034e51142587a3ba6f0f80445
barrientosvctor <vibarrientosp@gmail.com>
2023-06-16 23:29:06 -0500

    Updated code to new YouTube page

--- main.py
+++ main.py
@@ -2,28 +2,34 @@ from bs4 import BeautifulSoup
 from requests.api import get
 
 def main():
-    print('''
+    example_url = 'https://www.youtube.com/@SomeChannel'
+    print(f'''
 YouTube Channel ID Scraper
-Example: https://www.youtube.com/c/SomeChannel
+Example: {example_url}
     ''')
 
     inputUser = str(input('Type the URL of some YouTube channel to know its channel ID: '))
     try:
         result = get(inputUser)
-        soup = BeautifulSoup(result.text, 'lxml')
+        soup = BeautifulSoup(result.text, 'html.parser')
 
         # Channel information
-        title = soup.findAll(property='og:title')
-        channelId = soup.findAll(property='og:url')
+        fetch_name = soup.findAll(attrs={'itemprop': 'name'})
+        fetch_channel_id = soup.findAll(attrs={'itemprop': 'url'})
+
+        channel_name = str(fetch_name[0]).strip().split()[1].replace('content=', '').replace('"', '')
+        channel_url = str(fetch_channel_id[0]).strip().split()[1].replace('href=', '').replace('"', '')
+        channel_id = channel_url.split('/').pop()
 
         print(f'''
-    Name: {' '.join(str(e) for e in title).split('content=')[1].replace(' property="og:title"/>', '')}
-    Channel ID: {' '.join(str(e) for e in channelId).split('content=')[1].replace(' property="og:url"/>', '')}
+Name: {channel_name}
+Channel URL: {channel_url}
+Channel ID: {channel_id}
         ''')
     except ValueError:
-        print('Error: Must provide an URL from youtube.com to get YouTube Channel ID.\nExample: https://www.youtube.com/c/SomeChannel');
+        print(f'Error: Must provide an URL from youtube.com to get YouTube Channel ID.\nExample: {example_url}');
     except IndexError:
-        print('Error: YouTube Channel ID not found. Make sure that URL is from youtube.com\nExample: https://www.youtube.com/c/SomeChannel')
+        print(f'Error: YouTube Channel ID not found. Make sure that URL is from youtube.com\nExample: {example_url}')
 
 if __name__ == '__main__':
     main()
